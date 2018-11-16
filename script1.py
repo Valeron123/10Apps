11b2098d725f4d8a32e7e2d97244bd6d310e3dd1
Valerii <36187960+Valeron123@users.noreply.github.com>
2018-11-16 14:58:17 +0100

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+
+"""
+For building application I am going to use tkinter and SQLite
+
+"""
+
+#creating window
+window = Tk()
+
+
+
+
+window.mainloop()#without this window will be closed by itself
